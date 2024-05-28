4352b74b2a985d14051b9faff081ca85de36737c
Rishiyadav59 <150953062+Rishiyadav59@users.noreply.github.com>
2024-05-28 09:37:34 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
-import os 
-import sys
-
+import os , sys
 from sensor.logger import logging
+#from  sensor.utils import dump_csv_file_to_mongodb_collecton
+#from sensor.entity.config_entity  import TrainingPipelineConfig,DataIngestionConfig
 
-def test_exception():
-    try:
-        logging.info("ki yaha p bhaiaa ek error ayegi diveision by zero wali error ")
-        a=1/0
-    except Exception as e:
-       raise SensorException(e,sys) 
-      
+from sensor.pipeline.training_pipeline import TrainPipeline
 
+# def test_exception():
+#     try:
+#         logging.info("ki yaha p bhaiaa ek error ayegi diveision by zero wali error ")
+#         a=1/0
+#     except Exception as e:
+#        raise SensorException(e,sys) 
 
 
 
 if __name__ == "__main__":
 
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+    # try:
+    #     test_exception()
+    # except Exception as e:
+    #     print(e)
